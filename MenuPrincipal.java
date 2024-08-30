8eb45fd4301622b690ceef2d13d7d4c4680ef228
Paulo Vargas <pauloc1268gmail.com>
2024-08-30 17:56:18 -0600

    Vaciando Clases antiguas

--- MenuPrincipal.java
+++
@@ -1,9 +0,0 @@
-package n4.menuprincipal;
-
-public class MenuPrincipal {
-
-    public static void main(String[] args) {
-        MenuEventosGUI gui = new MenuEventosGUI();
-        gui.crearYMostrarGUI();
-    }
-}
